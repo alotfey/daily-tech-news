9a682d75a0741450c24a8b981714d3acd1a64287
alotfey <me@alotfey.com>
2022-01-17 20:15:13 -0500

    add page login ougout and session

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -43,11 +43,17 @@ router.post('/', (req, res) => {
       email: req.body.email,
       password: req.body.password
     })
-      .then(dbUserData => res.json(dbUserData))
-      .catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+    .then(dbUserData => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json(dbUserData);
       });
+    })
+    
+    
   });
 
   // login route
@@ -58,21 +64,39 @@ router.post('/', (req, res) => {
       }
     }).then(dbUserData => {
       if (!dbUserData) {
-        res.status(400).json({ message: 'wrong user or password' });
+        res.status(400).json({ message: 'No user with that email address!' });
         return;
       }
   
       const validPassword = dbUserData.checkPassword(req.body.password);
   
       if (!validPassword) {
-        res.status(400).json({ message: 'wrong user or password!' });
+        res.status(400).json({ message: 'Incorrect password!' });
         return;
       }
   
-      res.json({ user: dbUserData, message: 'You are now logged in!' });
+      req.session.save(() => {
+        // declare session variables
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+  
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
     });
   });
-
+  
+  // logout route
+  router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    }
+    else {
+      res.status(404).end();
+    }
+  });
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
       // pass in req.body instead to only update what's passed through
@@ -117,4 +141,6 @@ router.delete('/:id', (req, res) => {
   });
   
 
+  
+  
 module.exports = router;
--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,18 +1,28 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User } = require('../models');
+const { Post, User , Comment } = require('../models');
 
 router.get('/', (req, res) => {
+  console.log('======================');
   Post.findAll({
     attributes: [
       'id',
       'title',
       'post_content',
+      'created_at'
     ],
     include: [
       {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
         model: User,
-        attributes: ['username',]
+        attributes: ['username']
       }
     ]
   })
@@ -30,10 +40,26 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('login');
 });
+
+
+
 router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('signup');
 });
+
+
+
 
 module.exports = router;
